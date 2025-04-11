9dc7a3898f2e1b88c7f1884aacf6b147cea8c7a3
Ignacio Bravo <ignacioalberto48@gmail.com>
2025-04-11 11:39:54 -0400

    Add files via upload

---
+++ class02.py
@@ -0,0 +1,42 @@
+V01 = int(input("Ingresa un numero: "))
+V02 = float(input("dime tu numero decimal: "))
+
+
+# Operacion
+
+R01 = V01 + V02
+
+# salida
+
+print("El Resultado de la suma es: ", R01)
+
+
+#multiplicacion
+
+#resta 
+
+L01 = int(input("ingresa un numero: "))
+L02 = int(input("Dime otro numero para restar: "))
+
+#operacion 
+
+R2 = L01 - L02
+
+#salida
+
+print("El resultado es: ", R2)
+
+M1 = int(input("ingresa un digito: "))
+M2 = int(input("ingresa otro para que se multiplique: "))
+
+R3 = M1 * M2
+
+print("El resultado es: ", R3)
+
+
+D1 = int(input("agrega un numero: "))
+D2 = int(input("agrega otro para dividir: "))
+
+R4 = D1 /D2
+
+print("El resultado de la division es: ", R4)
